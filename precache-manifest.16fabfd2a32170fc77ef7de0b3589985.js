b63baa2b16649dc47d347135425e449b867660fe
kanin kearpimy <kanin23814@gmail.com>
2019-05-04 16:29:47 +0700

    remote

---
+++ precache-manifest.16fabfd2a32170fc77ef7de0b3589985.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fc2ca1ad1366d813111dd5461d3a823",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54992097d3efd7118530",
+    "url": "/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "54992097d3efd7118530",
+    "url": "/static/js/main.7a8aee5d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
